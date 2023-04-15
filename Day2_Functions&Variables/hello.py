07087fb6bd2fdabd7041a3aed5e70148194e4220
Monil_Karia <90109071+Monil-Karia@users.noreply.github.com>
2023-04-15 18:31:40 +0530

    Day2_Functions&Variables

---
+++ Day2_Functions&Variables/hello.py
@@ -0,0 +1,8 @@
+#Inundation is so important in python
+def hello(name="World"):
+    print(f"Hello {name}")
+
+hello() # it will print Hello world  as it will take default value name -> world 
+#Simple Program to take name and print it 
+name = input("What is ur name? ")
+hello(name)
---
+++ Day2_Functions&Variables/returnUsage.py
@@ -0,0 +1,8 @@
+def main():
+    x = int(input("Whats X? "))
+    print("Square of x is : " , square(x))
+
+def square(n):
+    return (n ** 2)
+
+main() 
