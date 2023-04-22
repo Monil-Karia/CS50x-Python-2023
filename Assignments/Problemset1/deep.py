884354e853074271a2d344447f624c378513de7f
Monil_Karia <90109071+Monil-Karia@users.noreply.github.com>
2023-04-22 13:54:09 +0530

    Problemset1 Codes added

---
+++ Assignments/Problemset1/deep.py
@@ -0,0 +1,6 @@
+deep = input("Great Question of Life, the Universe and Everything, ? ").lower()
+
+if(deep == "42" or deep == "forty-two" or deep == "forty two"):
+    print("Yes")
+else:
+    print("No")
