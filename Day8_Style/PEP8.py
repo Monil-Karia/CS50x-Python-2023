73b2a9d40ed375282a62c6e551d445d8d9938071
Monil_Karia <90109071+Monil-Karia@users.noreply.github.com>
2023-04-30 19:07:49 +0530

    Day8 Uploaded

---
+++ Day8_Style/PEP8.py
@@ -0,0 +1,29 @@
+# Python Enhancment Proposal  8 for formating the output for the user whixh is using Program
+# We make this beacuse we want the program to look good
+# "Readability Counts"
+
+"""
+1. Inendation 
+2. Tabs or Spaces
+3. Maximum Line Length
+4. Blank Lines
+5. Imports 
+...
+"""
+
+"""
+We use 'pylint' for the formating of the code 
+It is use to show the inundation errors or the normal error which are ralted to the formatting of the code 
+
+"""
+
+# LEts go to the Usage of the Program
+students = {
+    "Hermoine": "Griffindor",
+    "Harry": "Gryffindor",
+    "Ron": "GriffinDor",
+    "Draco": "Slytherin",
+    "Padma" : "Ravenclaw", 
+}
+for student in students:
+    print(student)
