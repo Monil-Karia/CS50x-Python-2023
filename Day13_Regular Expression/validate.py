73a7f5ee37fa75d00dbf171c5710756bb06367ba
Monil_Karia <90109071+Monil-Karia@users.noreply.github.com>
2023-06-13 11:01:58 +0530

    Delete Day13_Regular Expression directory

--- Day13_Regular Expression/validate.py
+++
@@ -1,8 +0,0 @@
-email = input("What's your email? ").strip()
-
-username , domain = email.split("@")
-
-if username and domain.endswith(".edu"):
-    print("Valid")
-else:
-    print("Invalid")
--- Day13_Regular Expression/validate_v2.py
+++
@@ -1,8 +0,0 @@
-import re
-
-email = input("What your Emaill? ")
-#.+ @ .+ means we assigned that after and before the @ we have strings that 
-if re.search(r"^\w+@(\w+\.)?\w+\.(edu|com|gov|net|org)$",email , re.IGNORECASE):
-    print("Valid")
-else:
-    print("Invalid")
