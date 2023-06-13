8edb1b592a69b384e0d9f5562e0ed4376f107b44
Monil_Karia <90109071+Monil-Karia@users.noreply.github.com>
2023-06-13 11:03:28 +0530

    Delete Day9_UnitTests directory

--- Day9_UnitTests/hello.py
+++
@@ -1,9 +0,0 @@
-def main():
-    name = input("Whaty yoour name? ")
-    print(hello(name))
-
-def hello(to="World"):
-    return(f"hello, {to}")
-
-if __name__ == "__main__":
-    main()
--- Day9_UnitTests/test/test_hello.py
+++
@@ -1,7 +0,0 @@
-from hello import hello
-
-def test_default():
-    assert hello() == "hello, World"
-
-def test_argument():
-    assert hello("David") =="hello, David"
