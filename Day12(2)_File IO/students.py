d1eef8721496d118e14c96acf29a9be902ed0adc
Monil_Karia <90109071+Monil-Karia@users.noreply.github.com>
2023-06-13 11:01:28 +0530

    Delete Day12(2)_File IO directory

--- Day12(2)_File IO/students.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-name  = input("Whats Your name? ")
-home  = input("Where's your home? ")
-
-with open("student_v2.csv" , "a") as file:
-    writer = csv.DictWriter(file , fieldnames=["name", "home"])
-    writer.writerow({"name" : name , "home": home})
