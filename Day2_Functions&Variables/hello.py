337e8cf3832c80da6d828f53f7cb72b0b59349d4
Monil_Karia <90109071+Monil-Karia@users.noreply.github.com>
2023-06-13 11:02:45 +0530

    Delete Day2_Functions&Variables directory

--- Day2_Functions&Variables/hello.py
+++
@@ -1,8 +0,0 @@
-#Inundation is so important in python
-def hello(name="World"):
-    print(f"Hello {name}")
-
-hello() # it will print Hello world  as it will take default value name -> world 
-#Simple Program to take name and print it 
-name = input("What is ur name? ")
-hello(name)
--- Day2_Functions&Variables/returnUsage.py
+++
@@ -1,8 +0,0 @@
-def main():
-    x = int(input("Whats X? "))
-    print("Square of x is : " , square(x))
-
-def square(n):
-    return (n ** 2)
-
-main() 
